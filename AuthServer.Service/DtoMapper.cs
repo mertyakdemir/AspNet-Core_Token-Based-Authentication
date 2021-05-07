7febfbf61699ec0cb0899da5004e3ab152362d8c
mertyakdemir <mertyakdemir07@gmail.com>
2021-05-07 22:15:19 +0300

    services

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServer.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
